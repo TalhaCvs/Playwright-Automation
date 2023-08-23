5cc647ffbc3280c0220e7576c92b74da8a9e1d90
TalhaCvs <talhacavus96@gmail.com>
2023-08-23 17:24:35 -0500

    deleted unnecessary imports

--- src/test/java/com/activeCampaign/utils/ConfigReader.java
+++ src/test/java/com/activeCampaign/utils/ConfigReader.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+///
 
 public class ConfigReader {
     public static String readProperty(String key){
